523fbb67d5d83f5177d49c2de5e17ab672de7366
Andy Blyler <andy@blyler.cc>
2026-07-17 12:11:51 -0400

    Fix release lint workflow

--- .version.sh
+++ .version.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ -z "$(git tag --points-at HEAD)" ]; then
+tags="$(git tag --points-at HEAD)"
+
+if [[ -z ${tags} ]]; then
 	git describe --always --long --dirty | sed 's/^v//'
 else
-	git tag --points-at HEAD | sed 's/^v//' | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | tail -n 1
+	printf '%s\n' "${tags}" | sed 's/^v//' | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | tail -n 1
 fi
